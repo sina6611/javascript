0e17c153c6a4428de14bfa696608cff5921b8717
Sina Barbaz Esfahani <sn.barbaz@gmail.com>
2022-04-18 14:11:50 +0200

    trainings

---
+++ script.js
@@ -0,0 +1,134 @@
+
+/*let i=1;
+while(i<=100){
+if(i==58){
+    i++
+    continue;
+}
+    i++;
+console.log(i)
+
+
+}
+
+let person={PName:"Sina", PFamilyname:"Barbaz Esfahani", PAge:34};
+for(let key in person){
+    console.log(key);
+    console.log(person[key])}
+
+const color=["red","blue", "Green"];
+for(let index in color){
+    console.log(index);
+    console.log(color[index]);
+}
+let i=40;
+do{
+    console.log(i);
+    i++
+}
+while (i<=49);
+
+let i=0;
+while( i<=50){
+
+    console.log(i)
+    i++;
+}
+
+for(let i=0;i<20;i++){
+
+if(i%2==0){
+    console.log(i)
+}    
+}
+
+let user="Sina";
+
+switch(user){
+
+    case "Geust":
+        console.log("User is a Geust");
+        break;
+    case "admin":
+        console.log("User is an admin");
+        break;
+    default:
+        console.log("Unknown User");
+
+
+
+}
+
+let uhr=13;
+
+if(uhr<12){
+
+    console.log("Good morning")
+
+}
+else if (uhr<20){
+    console.log("Good afternoon")
+}
+
+else {
+    console.log("Good neight")
+}
+
+
+let productPrice=12000;
+let count=5;
+let userCredit=1300;
+
+let buy=(count>0)||(userCredit>= productPrice);
+
+console.log(!buy);
+
+
+
+
+let firstname= "Sina" ;
+//console.log(firstname);
+typeof firstname;
+let person={pname:"Sina", pfamilyname:"Barbaz Esfahani", page:34};
+//console.log(person);
+//console.log(person.pname);
+//window.alert(person.pfamilyname)
+
+let users=["Sina","Mahdi"];
+users[2]="HGJJKK"
+function square(number){
+
+    return number*number;
+}
+let authenticated=false;
+let button=authenticated== true ? "dashboard": "log" ;
+console.log(button)
+
+let x=5;
+let y=23;
+console.log(y%=x);
+console.log(x*=y);
+console.log(x!=y)
+console.log(x==y)
+//console.log(square(20));
+//console.log(users);
+//console.log(users.length)
+//This is my firt java script file
+
+let a=34;
+let b=877;
+function add(c,d){
+z=c+d;
+console.log(z);
+}
+add(a,b);
+
+function greeting (firstname){
+
+console.log("Wellcome back" + " " + firstname)
+
+}
+
+greeting(firstname);
+
+*/
